49cf9a4c04e135c820ae7b5888367c1e4ffce21c
Tyler J. Wagner <tyler@tolaris.com>
2020-11-09 15:15:37 +0000

    Initial release.

---
+++ README.md
@@ -0,0 +1,59 @@
+# rename-via-sheets.py
+
+Given a set of source,destination filename pairs in a Google Sheet, rename the
+files.
+
+## Creating the Google Sheet
+
+Create your sheet with a header row in this format:
+
+| Source  | Destination |
+|---------|-------------|
+| foo.txt | bar.txt     |
+| baz.txt | qux.txt     |
+| qux.txt | baz.txt     |
+
+What is ignored:
+
+ * The first row, regardless of content
+ * Columns C and beyond, regardless of content
+ * Any row where cell A or B is empty
+
+## How renames are done
+
+Pseudocode:
+
+ * for each pair:
+  * generate a unique tempfile
+ * for each pair in row order from to bottom:
+  * rename source to tempfile
+  * rename tempfile to destination
+
+Therefore you can swap baz.txt and qux.txt without risk of overwriting each
+other.
+
+Renames are performed **relative to the current working directory**, using
+[os.replace()](https://docs.python.org/3/library/os.html#os.replace).
+
+## Example usage
+
+Dry run:
+
+```
+cd /path/to/files
+/home/tjwagner/Private/src/rename-via-sheet/rename-via-sheet.py -s
+1yaaa-aaaaaaaaaaaaaaaaaaaaaaaaaaaaa-aaaaaaaa -n
+```
+
+Actually rename:
+
+```
+/home/tjwagner/Private/src/rename-via-sheet/rename-via-sheet.py -s
+1yaaa-aaaaaaaaaaaaaaaaaaaaaaaaaaaaa-aaaaaaaa
+```
+
+## Getting a Google Sheets API key
+
+Follow the [Google Sheets Python API
+Quickstart](https://developers.google.com/sheets/api/quickstart/python) to
+generate an API key. Store it in credentials.json alongside the python script.
---
+++ rename-via-sheet.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Copyright 2020 Google LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+"""Rename files given a matrix in Google Sheets."""
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import argparse
+import pickle
+import os
+import os.path
+import sys
+import tempfile
+
+### defaults
+header = ["Source", "Destination"]
+# Update this to your Google Sheet, or pass in "-s". This one is mine.
+sheetsSpreadsheetId = "1yUS9-u19giB78IlwPXMhHqHzoiusAYcMyZ-8JgJDt2A"
+sheetsRangeName = "Sheet1!A2:B"
+# keep credentials in "$script_dir/credentials"
+credentialsFileName = os.path.join(os.path.dirname(sys.argv[0]), "credentials.json")
+credentialsTokenFileName = os.path.join(os.path.dirname(sys.argv[0]), "credentials_token.pickle")
+args = None
+
+def getSheetsService():
+  """Returns a Google Sheets service, refreshing credentials if required."""
+  # Taken directly from:
+  # https://developers.google.com/sheets/api/quickstart/python
+  scopes = ['https://www.googleapis.com/auth/spreadsheets']
+  creds = None
+  # The file token.pickle stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists(args.credentials_token):
+    with open(args.credentials_token, 'rb') as token:
+      creds = pickle.load(token)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+          creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(args.credentials, scopes)
+        creds = flow.run_local_server()
+      # Save the credentials for the next run
+      with open(args.credentials_token, 'wb') as token:
+          pickle.dump(creds, token)
+  return build('sheets', 'v4', credentials=creds)
+
+
+def getRenameList(spreadsheetId=sheetsSpreadsheetId,rangeName=sheetsRangeName):
+  """Read the Google Sheet and return a matrix {source,destination} filenames.
+
+  Ignores the first row, columns C and beyond, and any row with empty content for source or destination."""
+  # Call the Sheets API
+  service = getSheetsService()
+  request = service.spreadsheets().values().get(spreadsheetId=spreadsheetId,range=rangeName)
+  response = request.execute()
+  values = response.get('values', [])
+
+  # return only rows with 2 values (where both source and destination are populated)
+  return [ x for x in values if len(x) == 2 ]
+
+
+def renameFiles(filenames):
+  """Given a set of filenames as [source,destination] pairs, rename all files."""
+  # Avoids namespace collisions in source,destination sets by using intermediate
+  # temp files. Will overwrite any other destination files that may exist.
+  #
+  # pseudocode:
+  #   for each pair, generate a unique tempfile
+  #   for each pair, rename source to temp
+  #   for each pair, rename temp to destination
+  size = len(filenames)
+  source = size*[None]
+  destination = size*[None]
+  temp = size*[None]
+  if args.dry_run:
+    for src,dest in filenames:
+      print('Dry run, not renaming "{}" to "{}"'.format(src, dest))
+  else:
+    for i in range(size):
+      source[i] = filenames[i][0]
+      destination[i] = filenames[i][1]
+      temp[i] = tempfile.NamedTemporaryFile(delete=False,dir=os.getcwd()).name
+      print('Renaming "{}" to "{}"'.format(source[i], destination[i]))
+    for i in range(size):
+      os.replace(src=source[i],dst=temp[i])
+    for i in range(size):
+      os.replace(src=temp[i],dst=destination[i])
+
+
+if __name__ == '__main__':
+  # argparse
+  parser = argparse.ArgumentParser(description=__doc__)
+
+  parser.add_argument("-s", "--sheet", help="Google Sheet ID to read (default {})".format(sheetsSpreadsheetId), default=sheetsSpreadsheetId)
+  parser.add_argument("-r", "--range", help="range to read (default {})".format(sheetsRangeName), default=sheetsRangeName)
+  parser.add_argument("-c", "--credentials", help="path to credentials file (default {})".format(credentialsFileName), default=credentialsFileName)
+  parser.add_argument("-t", "--credentials_token", help="path to credentials token (default {})".format(credentialsTokenFileName), default=credentialsTokenFileName)
+  parser.add_argument("-n", "--dry-run", action="store_true", help="dry run, don't write changes")
+  args = parser.parse_args()
+
+  renameFiles(getRenameList())
