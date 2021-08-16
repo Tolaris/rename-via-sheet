6b1c5ea9ded661c8cf8d585fdd4b72966d5fa6bd
Tyler J. Wagner <tyler@tolaris.com>
2021-08-16 22:20:48 +0100

    Add apt-get hints to source.

--- rename-via-sheet.py
+++ rename-via-sheet.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# apt-get install python3-googleapi python3-google-auth-oauthlib
+
 """Rename files given a matrix in Google Sheets."""
 
 from googleapiclient.discovery import build
